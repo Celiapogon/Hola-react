83226b40b82438452543fef76075e5ade092c901
Celia Poza <celiapg96.cp@gmail.com>
2025-02-11 17:57:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ToDoList from './ToDoList';
-import Calculadora from "./Calculadora"
 
 import './ToDoList.css';
 import BlogDeNotas from './BlogDeNotas';
